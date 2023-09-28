30bdf180626b840e6e4411b7225ae235d6258b99
華帆 <mkaho0919kaho.wk@gmail.com>
2023-09-28 22:49:51 +0900

    実績の入力を削除

--- popup.js
+++ popup.js
@@ -36,34 +36,34 @@ const blueBtn = document.querySelector('.btn--blue');
 let value1 = ""
 let value2 = ""
 
-const report = 
-"＜今日やろうとしていたこと＞\n●AP計画タスク\n\n●計画外作業\n\n＜今日実際にやったこと(やれたこと)＞\n●AP計画タスク\n\n●計画外作業\n\n＜天気（こころとからだの余裕）＞\nはれ\n\n＜その他(気づき等)＞\nなし"
+// const report = 
+// "＜今日やろうとしていたこと＞\n●AP計画タスク\n\n●計画外作業\n\n＜今日実際にやったこと(やれたこと)＞\n●AP計画タスク\n\n●計画外作業\n\n＜天気（こころとからだの余裕）＞\nはれ\n\n＜その他(気づき等)＞\nなし"
 
-yellowBtn.addEventListener('click', () => { 
-  value1 = document.querySelector('.form-control1').value;
-  value2 = document.querySelector('.form-control2').value;
+// yellowBtn.addEventListener('click', () => { 
+//   value1 = document.querySelector('.form-control1').value;
+//   value2 = document.querySelector('.form-control2').value;
   
-  const arrayValue1 = value1.split('\n');
-  const arrayValue2 = value2.split('\n');
+//   const arrayValue1 = value1.split('\n');
+//   const arrayValue2 = value2.split('\n');
 
-  let newArray1 = [];
-  for (let i = 0; i < arrayValue1.length; i++) {
-    newArray1.push(arrayValue1[i] + ":　h\n");
-  }
-  let newArray2 = [];
-  for (let i = 0; i < arrayValue2.length; i++) {
-    newArray2.push(arrayValue2[i] + ":　h\n");
-  }
-  const newValue1 = newArray1.join("");
-  const newValue2 = newArray2.join("");
+//   let newArray1 = [];
+//   for (let i = 0; i < arrayValue1.length; i++) {
+//     newArray1.push(arrayValue1[i] + ":　h\n");
+//   }
+//   let newArray2 = [];
+//   for (let i = 0; i < arrayValue2.length; i++) {
+//     newArray2.push(arrayValue2[i] + ":　h\n");
+//   }
+//   const newValue1 = newArray1.join("");
+//   const newValue2 = newArray2.join("");
 
-  const reporttt = `＜今日やろうとしていたこと＞\n●AP計画タスク\n${value1}\n\n●計画外作業\n${value2}\n\n＜今日実際にやったこと(やれたこと)＞\n●AP計画タスク\n${newValue1}\n●計画外作業\n${newValue2}\n＜天気（こころとからだの余裕）＞\nはれ\n\n＜その他(気づき等)＞\nなし`
+//   const reporttt = `＜今日やろうとしていたこと＞\n●AP計画タスク\n${value1}\n\n●計画外作業\n${value2}\n\n＜今日実際にやったこと(やれたこと)＞\n●AP計画タスク\n${newValue1}\n●計画外作業\n${newValue2}\n＜天気（こころとからだの余裕）＞\nはれ\n\n＜その他(気づき等)＞\nなし`
 
-  navigator.clipboard.writeText(reporttt); 
+//   navigator.clipboard.writeText(reporttt); 
 
-  yellowBtn.innerHTML = 'OK!'; 
-  setTimeout(() => (yellowBtn.innerHTML = 'Copy'), 1000);
-});
+//   yellowBtn.innerHTML = 'OK!'; 
+//   setTimeout(() => (yellowBtn.innerHTML = 'Copy'), 1000);
+// });
 
 redBtn.addEventListener('click', () => { 
   navigator.clipboard.writeText(start.textContent); 
